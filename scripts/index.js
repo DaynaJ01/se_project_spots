eebac93f9818300be5f4418015c3de7c7c01deab
DaynaJ01 <daynajones01@gmail.com>
2026-03-31 17:44:16 +0200

    stage 8 changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,8 @@
 const initialCards = [
+        {
+        name: "Golden Gate Bridge",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg"},
+    
     {
         name: "Val Thorens",
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"},
@@ -52,6 +56,12 @@ const newPostImageInput = newPostModal.querySelector("#card-image-input");
 
 const newPostCaptionInput = newPostModal.querySelector("#profile-description-input");
 
+const previewModal = document.querySelector("#preview-modal");
+const previewModalCloseBtn = previewModal.querySelector(".modal__close-btn");
+const previewImageEl = previewModal.querySelector(".modal__image");
+const previewImageCaption = previewModal.querySelector(".modal__caption");
+
+
 
 function openModal(modal){
     modal.classList.add("modal_is-opened");
@@ -61,13 +71,49 @@ function closeModal(modal){
     modal.classList.remove("modal_is-opened");
 }
 
+previewModalCloseBtn.addEventListener("click", function () {
+    closeModal(previewModal);
+})
+
+
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
-const newPostImageLink = newPostImageInput.value;
-const newPostCaption = newPostCaptionInput.value;
 
 
+const cardTemplate = document
+    .querySelector("#card-template")
+    .content.querySelector(".card");
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardTitleEl = cardElement.querySelector(".card__title");
+    const cardImageEl = cardElement.querySelector(".card__image");
+
+    cardImageEl.src = data.link;
+    cardImageEl.alt = data.name;
+    cardTitleEl.textContent = data.name;
+
+    const cardLikeBtnEl = cardElement.querySelector(".card__like-btn");
+    cardLikeBtnEl.addEventListener("click", () =>{
+        cardLikeBtnEl.classList.toggle("card__like-btn_active");
+    });
+
+    const cardDeleteBtnEl = cardElement.querySelector(".card__delete-button");
+    cardDeleteBtnEl.addEventListener("click", () => {
+        cardDeleteBtnEl.closest(".card").remove();
+    })
+
+    cardImageEl.addEventListener("click", () => {
+        previewImageEl.src = data.link;
+        previewImageEl.alt = data.name;
+        previewImageEl.textContent = data.name;
+        openModal(previewModal);
+    })
+
+    return cardElement;
+}
 
 
 editProfileBtn.addEventListener("click", function () {
@@ -94,17 +140,6 @@ newPostCloseBtn.addEventListener("click", function () {
 
 });
 
-function handleProfileFormSubmit(evt) {
-  // Prevent default browser behavior.
-  evt.preventDefault(); 
-
-  profileNameEl.textContent = editProfileNameInput.value;
-
-  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
-
-  //editProfileModal.classList.remove("modal_is-opened");
-  closeModal(editProfileModal);
-}
 
 // Set the submit listener.
 editProfileFormEl.addEventListener('submit', handleProfileFormSubmit);
@@ -131,8 +166,22 @@ function handleAddCardSubmit(evt) {
   evt.preventDefault(); 
  
   // Log both input values to the console.
-  console.log(newPostImageLink);
-  console.log(newPostCaption);
+  const newPostImageLink = newPostImageInput.value;
+  const newPostCaptionLink = newPostCaptionInput.value;
+
+  const inputValues = {
+    name: newPostCaptionLink,
+    link: newPostImageLink,
+  }
+
+  
+  //console.log(newPostImageLink.value);
+  //console.log(newPostCaptionLink.value);
+  const cardElement = getCardElement({
+    name: newPostCaptionLink,
+    link: newPostImageLink,
+});
+  cardsList.prepend(cardElement);
 
 
   // Close the modal.
@@ -144,8 +193,8 @@ function handleAddCardSubmit(evt) {
 newPostEl.addEventListener('submit', handleAddCardSubmit);
 
 
-initialCards.forEach(function (card) {
-    console.log(card.name);
-    console.log(card.link);
+initialCards.forEach(function (item) {
+    const cardElement = getCardElement(item);
+    cardsList.append(cardElement);
 });
 
